d9153d862b4beb7c11177120bc0574059668c4c8
Elias Schwalme <132968734+eliasschwalme@users.noreply.github.com>
2024-02-03 13:35:05 +0100

    Update text2speech.py

--- src/utils/text2speech.py
+++ src/utils/text2speech.py
@@ -14,6 +14,7 @@ class TTSTalker():
         tempf  = tempfile.NamedTemporaryFile(
                 delete = False,
                 suffix = ('.'+'wav'),
+                suffix = ('.'+'wav_Lux'),
             )
 
         self.tts.tts_to_file(text, speaker=self.tts.speakers[0], language=language, file_path=tempf.name)
